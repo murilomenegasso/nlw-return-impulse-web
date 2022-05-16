827fe94737589f114d5e8b423cffff9ee81998b6
Methodos Tecnologia <murilo@methodostecnologia.com.br>
2022-05-16 09:13:43 -0300

    NLW-RETURN-WEB commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Widget } from "./components/Widget";
+
+export function App(){
+  return <Widget />
+}
+
+
+// interface ButtonProps{
+//   text?:string;
+// }
+
+// function Button(props: ButtonProps){
+//   //return <button className="bg-violet-500 px-4 h-10 rounded">{props.text??'default'}</button>
+//   return <button className="button">{props.text??'default'}</button>
+// }
+
+// function App() {
+//   return (
+//     <div className="flex gap-2">
+//       <Button text="Enviar"/>
+//       <Button text="Ok"/>
+//     </div>
+//   )
+// }
+
+// export default App
